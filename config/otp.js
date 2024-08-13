a68094aa2b3f7282ce1005e5f78a88a906f96826
Omolarry <90278887+Omolarry@users.noreply.github.com>
2024-08-13 08:49:09 +0100

    Update otp.js

--- config/otp.js
+++ config/otp.js
@@ -1,24 +1,25 @@
-const nodemailer = require("nodemailer");
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
-const sendOtpEmail = async (email, otp) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
+// Configure API key authorization: api-key
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.BREVO_API_KEY;
 
+const sendOTPEmail = async (recipientEmail, otp) => {
+    let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-    const mailOptions = {
-        from: "Oluwaseyi Akintoye <seyiakintoye7@gmail.com>",
-        to: email,
-        subject: "OTP Verification(valid for 5 minutes)",
-        text: `Your OTP is: ${otp}`
+    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    sendSmtpEmail.to = [{ email: recipientEmail }];
+    sendSmtpEmail.sender = { email: 'seyiakintoye7@gmail.com', name: 'Tomicare' }; // Replace with your verified sender
+    sendSmtpEmail.subject = 'Your OTP Code(valid for 5 minutes.)';
+    sendSmtpEmail.textContent = `Your OTP code is: ${otp}`;
+
+    try {
+        await apiInstance.sendTransacEmail(sendSmtpEmail);
+    } catch (error) {
+        console.error('Error sending OTP email:', error);
     }
-
-    await transporter.sendMail(mailOptions);
 };
 
-module.exports = sendOtpEmail;
+
+module.exports = sendOTPEmail;
